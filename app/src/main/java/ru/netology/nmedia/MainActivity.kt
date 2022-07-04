d7a422c54fe7bd76c7565599200c205aee176e36
Vladimir Chistiakov <stomalux@yandex.ru>
2022-07-05 02:14:14 +1000

    05-07 изменен gradle, добавлен Post, сердечко нажимается.

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,11 +1,39 @@
 package ru.netology.nmedia
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.netology.nmedia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)                       //(R.layout.activity_main)
+
+        var post = Post(
+            share = 0,
+            allLiked = 333333333333
+        )
+
+        binding.netology.text =post.rr
+        binding.favorite.setOnClickListener {
+            post.liked = !post.liked
+            binding.favorite.setImageResource(
+                if (post.liked) {
+                    post.allLiked ++
+
+
+
+                    R.drawable.ic_baseline_favorite_24
+
+
+
+                } else {
+                    post.allLiked --
+                    R.drawable.ic_baseline_favorite_border_24
+                }
+            )
+        }
     }
 }
---
+++ app/src/main/java/ru/netology/nmedia/Post.kt
@@ -0,0 +1,10 @@
+package ru.netology.nmedia
+
+data class Post(
+    val id: Long = 0,
+    val share: Long,
+    var liked: Boolean = false,
+    var allLiked: Long,
+    var rr: String = "Нетология. Университет интернет-профессий."
+
+)
