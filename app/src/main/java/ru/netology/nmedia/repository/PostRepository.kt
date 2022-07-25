db3d745be2a63e62ffb945edd24322c005e656a9
Vladimir Chistiakov <stomalux@yandex.ru>
2022-07-26 00:44:54 +1000

    Перед Фрагментами. Пока все работает

--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -6,10 +6,11 @@ import ru.netology.nmedia.dto.Post
 interface PostRepository {
     fun getAll(): LiveData<List<Post>>
     fun likeById(id: Long)
-    fun save(it: Post)
+    fun save(post: Post)
     fun removeById(id: Long)
 
     fun sharesById(id: Long)
+
 
 
 }
