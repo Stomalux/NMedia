d6db0a2636235746a493c8739fe7f723dc5ab314
Vladimir Chistiakov <stomalux@yandex.ru>
2022-08-01 02:33:42 +1000

    Рефакторинг Activity -> Fragment слайды 24 - 29

--- app/src/main/java/ru/netology/nmedia/activity/FeedFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/FeedFragment.kt
@@ -5,45 +5,59 @@ package ru.netology.nmedia.activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import android.view.View.VISIBLE
+import android.view.ViewGroup
 import androidx.activity.result.launch
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import ru.netology.nmedia.R
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.adapter.OnInteractionListener
 import ru.netology.nmedia.adapter.PostsAdapter
-import ru.netology.nmedia.databinding.ActivityMainBinding
+//import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.databinding.FragmentFeedBinding
 
 import ru.netology.nmedia.viewmodel.PostViewModel
 
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+class FeedFragment : Fragment() {
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    val viewModel by viewModels<PostViewModel>()
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    val newPostLauncher = registerForActivityResult(NewPostActivityContract()) { text ->
+        text ?: return@registerForActivityResult
+        viewModel.changeContent(text.toString())
+        viewModel.save()
+    }
 
-        val viewModel by viewModels<PostViewModel>()
-
-       val newPostLauncher = registerForActivityResult(NewPostActivityContract()) { text ->
-            text ?: return@registerForActivityResult
+    val editPostLauncher = registerForActivityResult(EditPostActivityContract()) { text ->
+        text?.let {                                         //: return@registerForActivityResult
             viewModel.changeContent(text.toString())
-               viewModel.save()
-        }
-
-        val editPostLauncher = registerForActivityResult(EditPostActivityContract()) { text ->
-            text?.let {                                         //: return@registerForActivityResult
-              viewModel.changeContent(text.toString())
-              //  viewModel.changeContent(it)
-                viewModel.save()
-            }
-        }
-        val intentHandlerLauncher = registerForActivityResult(IntentHadlerActivityContract()) { text ->
-            text ?: return@registerForActivityResult
-            viewModel.changeContent(text.toString())
+            //  viewModel.changeContent(it)
             viewModel.save()
         }
+    }
+    val intentHandlerLauncher = registerForActivityResult(IntentHadlerActivityContract()) { text ->
+        text ?: return@registerForActivityResult
+        viewModel.changeContent(text.toString())
+        viewModel.save()
+    }
+ ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentFeedBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         val adapter = PostsAdapter(object : OnInteractionListener {
 
 
@@ -64,7 +78,7 @@ class MainActivity : AppCompatActivity() {
 
             override fun onShare(post: Post) {
                 viewModel.sharesById(post.id)
-                    ////////////////////////////////////////////////////////////////////////
+                ////////////////////////////////////////////////////////////////////////
                 val intent = Intent().apply {
                     action = Intent.ACTION_SEND
                     putExtra(Intent.EXTRA_TEXT, post.content)
@@ -87,18 +101,29 @@ class MainActivity : AppCompatActivity() {
             }
         })
         binding.list.adapter = adapter
-        viewModel.data.observe(this)
+        viewModel.data.observe(viewLifecycleOwner)
         { posts ->
             val newPost = adapter.itemCount < posts.size
             adapter.submitList(posts) {
                 if (newPost) binding.list.smoothScrollToPosition(0)
             }
         }
+
         binding.create.setOnClickListener {
             newPostLauncher.launch()
         }
 
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        return binding.root
     }
 }
+
+
+
+
+
+
+
+
+
 
