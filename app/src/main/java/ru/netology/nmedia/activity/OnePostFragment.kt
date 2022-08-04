c53ea1e4ca99328bdf7e40cf2c9ef91bca403ba9
Vladimir Chistiakov <stomalux@yandex.ru>
2022-08-04 22:53:51 +1000

    Фрагменты создается онеПост. перед обновлением градла. нахожусь на OnePostFragment.kt 39

--- app/src/main/java/ru/netology/nmedia/activity/OnePostFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/OnePostFragment.kt
@@ -12,10 +12,14 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import ru.netology.nmedia.R
+import ru.netology.nmedia.activity.NewPostFragment.Companion.textArg
+import ru.netology.nmedia.activity.OnePostFragment
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.adapter.OnInteractionListener
+import ru.netology.nmedia.adapter.PostViewHolder
 import ru.netology.nmedia.adapter.PostsAdapter
 //import ru.netology.nmedia.databinding.ActivityMainBinding
 import ru.netology.nmedia.databinding.FragmentNewPostBinding
@@ -27,23 +31,91 @@ import ru.netology.nmedia.viewmodel.PostViewModel
 
 class OnePostFragment : Fragment() {
 
-    val viewModel by viewModels<PostViewModel>()
- //   private val args: OnePostFragment by navArgs<OnePostFragment>()
+  //  val viewModel by viewModels<PostViewModel>()
+    val viewModel by viewModels<PostViewModel>(ownerProducer = ::requireParentFragment)
+    //   private val args: OnePostFragment by navArgs<OnePostFragment>()
+
+
+    val args by navArgs<OnePostFragment>()
+
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         val binding = FragmentPostScrollBinding.inflate(
             inflater,
             container,
             false
-
         )
+        val viewHolder = PostViewHolder(binding.postDetail, object : OnInteractionListener {
 
-      //  binding.content.setOnClickListener {
+
+//            override fun  onHandler(post: Post){
+//                viewModel.edit(post)
+//                intentHandlerLauncher.launch(post.content)
+//
+//
+//            }
+//            override fun onEdit(post: Post) {
+//                viewModel.edit(post)
+//                editPostLauncher.launch(post.content)
+//            }
+
+            override fun onLike(post: Post) {
+                viewModel.likeById(post.id)
+            }
+
+            override fun onShare(post: Post) {
+                viewModel.sharesById(post.id)
+                ////////////////////////////////////////////////////////////////////////
+                val intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, post.content)
+                    type = "text/plain"
+                }
+
+                val shareIntent =
+                    Intent.createChooser(intent, getString(R.string.chooser_share_post))
+                startActivity(shareIntent)
+                /////////////////////////////////////////////////////////////////////////////
+            }
+
+            override fun onRemove(post: Post) {
+                viewModel.removeById(post.id)
+            }
+
+            override fun onVideo(post: Post) {
+                val intentVideo = Intent(Intent.ACTION_VIEW, Uri.parse(post.video))
+                startActivity(intentVideo)
+            }
+
+            override fun onContent(post: Post) {
+                findNavController().navigate(R.id.action_feedFragment_to_onePostFragment)
+            }
+            override fun onEdit(post: Post) {
+                viewModel.edit(post)
+                findNavController().navigate(
+                    R.id.action_feedFragment_to_editPostFragment,
+                    Bundle().apply {
+                        textArg = post.content
+                    }
+                )
+            }
 
 
+
+        } )
+
+
+        viewHolder.bind()
+
+        return binding.root
+
+
+        //  binding.content.setOnClickListener {
 
 
 //        val newPostLauncher = registerForActivityResult(NewPostActivityContract()) { text ->
@@ -121,13 +193,13 @@ class OnePostFragment : Fragment() {
 //                if (newPost) binding.postDetail.smoothScrollToPosition(0)
 //            }
 //        }
-      //  binding.con.setOnClickListener {
-          //  newPostLauncher.launch()
-            return binding.root
-        }
-
-
+        //  binding.con.setOnClickListener {
+        //  newPostLauncher.launch()
+        return binding.root
     }
+
+
+}
 
 
 
